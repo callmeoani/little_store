df3468adcf5b8dfc525f0529d045c8e734c069c3
oani <callmeoani@gmail.com>
2025-04-07 08:59:56 +0100

    first commit: project creation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-basics' title`, () => {
+  it(`should have the 'my-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-basics');
+    // expect(app.title).toEqual('my-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-basics');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, my-app'
+    );
   });
 });
